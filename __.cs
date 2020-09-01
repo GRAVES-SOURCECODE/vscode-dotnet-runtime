8451b650aee066cbaf3d90c89c304e61b8017692
Graves <GRAV023@ALSTECHNOLOGY.NET>
2020-09-01 12:52:58 -0400

    Committ

---
+++ __.cs
@@ -0,0 +1,17 @@
+var @params = new NameValueCollection
+{
+    //Azure AD will return an authorization code. 
+    //See the Redirect class to see how "code" is used to AcquireTokenByAuthorizationCode
+    {"response_type", "code"},
+
+    //Client ID is used by the application to identify themselves to the users that they are requesting permissions from.
+    //You get the client id when you register your Azure app.
+    {"client_id", Properties.Settings.Default.ClientID},
+
+    //Resource uri to the Power BI resource to be authorized
+    // https://analysis.windows.net/powerbi/api
+    {"resource", Properties.Settings.Default.PowerBiAPI},
+
+    //After user authenticates, Azure AD will redirect back to the web app
+    {"redirect_uri", "https://localhost:13526/Redirect"}
+};
